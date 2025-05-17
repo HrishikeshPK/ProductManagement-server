9bb43ad1d7dc07c8c3dfd1d45a22f84e37762d76
Hrishikesh P K <hrishilovjith@gmail.com>
2025-05-17 13:47:07 +0530

    Product edit and deleted api tested

--- controllers/productController.js
+++ controllers/productController.js
@@ -42,3 +42,45 @@ exports.getProducts = async (req, res) => {
       .json({ msg: "Error fetching products", error: err.message });
   }
 };
+
+exports.updateProduct = async (req, res) => {
+  const { name, subCategoryId, variants } = req.body;
+
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) return res.status(404).json({ msg: "Product not found" });
+
+    if (name && name !== product.name) {
+      const existing = await Product.findOne({ name: name.trim() }); // Check for duplicate name (if name is changing)
+      if (existing)
+        return res
+          .status(400)
+          .json({ msg: "Product with this name already exists" });
+      product.name = name.trim();
+    }
+
+    if (subCategoryId) product.subCategory = subCategoryId;
+
+    if (variants && (!Array.isArray(variants) || variants.length === 0)) {
+      return res.status(400).json({ msg: "At least one variant is required." }); //  validate variants on update
+    }
+
+    if (variants) product.variants = variants;
+
+    await product.save();
+    res.json({ msg: "Product updated", product });
+  } catch (err) {
+    res.status(500).json({ msg: "Error updating product", error: err.message });
+  }
+};
+
+exports.deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if (!product) return res.status(404).json({ msg: "Product not found" });
+
+    res.json({ msg: "Product deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ msg: "Error deleting product", error: err.message });
+  }
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { createProduct, getProducts } = require('../controllers/productController');
+const { createProduct, getProducts,updateProduct,deleteProduct } = require('../controllers/productController');
 const auth = require('../middlewares/authMiddleware');
 const adminOnly = require('../middlewares/admin');
 
+
 router.post('/', auth,adminOnly, createProduct); // only authorised users can access
+router.put("/:id", auth, adminOnly, updateProduct);// only authorised users can access
+router.delete("/:id", auth, adminOnly, deleteProduct);// only authorised users can access
 router.get('/', getProducts);         // not authorised
 
 module.exports = router;
